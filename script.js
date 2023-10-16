46dc8819f9ce9d7838588af7e1d20b3a2eef2721
Duduhiro <duduhiro0312@gmail.com>
2023-10-15 23:21:24 -0300

    Buttons now display numbers on the screen

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+let screenValue = ''
 
+function showOnScreen (value) {
+    if (screenValue.length < 14) {
+        const screen = document.getElementById('screen')
+        screen.innerHTML += value
+        screenValue += value
+    }
+}
+
+function clearScreen () {
+    const screen = document.getElementById('screen')
+    screen.innerHTML = ''
+    screenValue = ''
+}
 
 function sum(a, b) {
     return (a + b)
