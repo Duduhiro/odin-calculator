e17f275d6d0be720dba9ace8a938eb56c700e506
Duduhiro <duduhiro0312@gmail.com>
2023-10-16 01:02:38 -0300

    Implemented the equal function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let screenValue = ''
-
 let currentValue = 0
+let lastOp = 0 // 1- add | 2- sub | 3- mult | 4- div
 
 function showOnScreen (value) {
     if (screenValue.length < 14) {
@@ -36,13 +36,22 @@ function deleteNumber () {
 
 function executeSum() {
     if (currentValue === 0) {
-        currentValue = parseInt(screenValue)
-    } else {
         currentValue += parseInt(screenValue)
-        showResult(currentValue)
+        clearScreen()
+        lastOp = 1
+    } else {
+        showResult(currentValue + parseInt(screenValue))
     }
 }
 
 function executeResult () {
-    showResult(currentValue)
+    if (lastOp === 1) {
+        showResult(currentValue + parseInt(screenValue))
+    } else if (lastOp === 2) {
+        showResult(currentValue - parseInt(screenValue))
+    } else if (lastOp === 3) {
+        showResult(currentValue * parseInt(screenValue))
+    } else if (lastOp === 4) {
+        showResult(currentValue / parseInt(screenValue))
+    }
 }
