defb626b1c84b7166d764fb03616ff691491b157
Duduhiro <duduhiro0312@gmail.com>
2023-10-17 23:00:49 -0300

    Used the same logic to the other math operations

--- script.js
+++ script.js
@@ -41,10 +41,70 @@ function executeSum() {
     }
 }
 
+function executeSub() {
+    const sumButton = document.querySelector('#sum')
+    operation = 2
+
+    if (starterValue == null) {
+        updateFirstValue(displayValue)
+    }
+    clearScreen()
+    if (secondValue != null) {
+        executeResult()
+    }
+}
+
+function executeMult() {
+    const sumButton = document.querySelector('#sum')
+    operation = 3
+
+    if (starterValue == null) {
+        updateFirstValue(displayValue)
+    }
+    clearScreen()
+    if (secondValue != null) {
+        executeResult()
+    }
+}
+
+function executeDiv() {
+    const sumButton = document.querySelector('#sum')
+    operation = 4
+
+    if (starterValue == null) {
+        updateFirstValue(displayValue)
+    }
+    clearScreen()
+    if (secondValue != null) {
+        executeResult()
+    }
+}
+
 function executeResult() {
     if (operation == 1) {
         secondValue = parseInt(displayValue)
         starterValue += secondValue
+        secondValue = null
+        clearScreen()
+        updateDisplay(starterValue)
+    }
+    if (operation == 2) {
+        secondValue = parseInt(displayValue)
+        starterValue -= secondValue
+        secondValue = null
+        clearScreen()
+        updateDisplay(starterValue)
+    }
+    if (operation == 3) {
+        secondValue = parseInt(displayValue)
+        starterValue *= secondValue
+        secondValue = null
+        clearScreen()
+        updateDisplay(starterValue)
+    }
+    if (operation == 4) {
+        secondValue = parseInt(displayValue)
+        starterValue /= secondValue
         secondValue = null
         clearScreen()
         updateDisplay(starterValue)
